b195d2cd53b84498ca90f076f09ac6e67e035e40
Jefferson Shibuya <jeffersonshibuya@yahoo.com.br>
2020-05-09 12:20:32 -0300

    add heroku baseURL hardcoded

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 const axios = require("axios");
 
 const api = axios.create({
-  baseURL: process.env.API || "http://localhost:3333",
+  // baseURL: process.env.API || "http://localhost:3333",
+  baseURL: "https://app-radar-dev.herokuapp.com/",
 });
 
 export default api;
